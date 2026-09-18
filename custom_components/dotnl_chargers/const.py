9079d91bf9526192b60ba16f6842500eaabda405
DonTranQuiL <dontranquil@users.noreply.github.com>
2026-09-18 06:05:28 +0000

    chore: bump version to 0.1.3
    v0.1.2 is already published. This revision is the map-marker cap
    cleanup that deletes dropped charger pins instead of leaving them
    unavailable.

--- custom_components/dotnl_chargers/const.py
+++ custom_components/dotnl_chargers/const.py
@@ -7,7 +7,7 @@ from homeassistant.const import Platform
 DOMAIN = "dotnl_chargers"
 NAME = "DOT-NL Chargers"
 MANUFACTURER = "DonTranQuiL"
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 ATTRIBUTION = "Data © NDW / DOT-NL (AFIR open data)"
 
 PLATFORMS: list[Platform] = [
